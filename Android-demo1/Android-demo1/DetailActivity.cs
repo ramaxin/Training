4dd6b6cfe8d3c36da7586f09373a766abeed7e75
MY-PC\Pham Thanh Vo <ptvo98@gmail.com>
2017-02-13 09:05:24 +0700

    Android demo 1

---
+++ Android-demo1/Android-demo1/DetailActivity.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Android_demo1
+{
+    [Activity(Label = "DetailActivity")]
+    public class DetailActivity : Activity
+    {
+        private TextView tvDetail;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Detail);
+            tvDetail = FindViewById<TextView>(Resource.Id.tvDetail);
+            string text = Intent.GetStringExtra("p1") ?? "Not found";
+
+            tvDetail.Text = text;
+            // Create your application here
+        }
+    }
+}
---
+++ Android-demo1/Android-demo1/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using System;
+
+namespace Android_demo1
+{
+    [Activity(Label = "Android_demo1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private EditText txtTest;
+        private TextView tvTest;
+        private Button btnTest;
+        private Button btnDetail;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            txtTest = FindViewById<EditText>(Resource.Id.txtTest);
+            tvTest = FindViewById<TextView>(Resource.Id.lblTest);
+            btnTest = FindViewById<Button>(Resource.Id.btnTest);
+            btnDetail = FindViewById<Button>(Resource.Id.btnDetail);
+            btnTest.Click += myButtonClick;
+            btnDetail.Click += myDetailClick;
+
+        }
+
+        private void myDetailClick(object sender, EventArgs e)
+        {
+            string vDetail = txtTest.Text;
+            var intend = new Intent(this,typeof(DetailActivity));
+            intend.PutExtra("p1", vDetail);
+            StartActivity(intend);
+        }
+
+        private void myButtonClick(object sender, EventArgs e)
+        {
+            tvTest.Text = txtTest.Text;
+        }
+    }
+}
+
